47f2726e00bd0ab5a97e239e66ddc42b48a91c80
Artem Epifanov <dizzydw@yandex.ru>
2025-04-09 21:52:24 +0300

    Добавил ClearML

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import argparse
 import pytorch_lightning as pl
-import datetime, dateutil.tz
+import datetime
+import dateutil.tz
+from clearml import Task
 import mlpt.config.config as cfg
 from mlpt.modules.train import GANLitModule
 from mlpt.datamodules.datamodule import DeepFashionDataModule
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Train GAN network on DeepFashion dataset using PyTorch Lightning")
+    parser = argparse.ArgumentParser(
+        description="Train GAN network on DeepFashion dataset using PyTorch Lightning")
     parser.add_argument('--gpu', dest='gpu_id', type=str, default='0')
     parser.add_argument('--data_dir', dest='data_dir', type=str, default='')
     parser.add_argument('--train', dest='train', type=str, default="y")
@@ -14,8 +18,12 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = parse_args()
+
+    task = Task.init(project_name="DeepFashion GAN",
+                     task_name="Trying ClearML")
 
     if args.gpu_id != '-1':
         cfg.GPU_ID = args.gpu_id
@@ -30,7 +38,8 @@ if __name__ == "__main__":
     if args.train == "y":
         cfg.TRAIN_FLAG = True
     else:
-        cfg.NET_G = "../data/models/netG_epoch_360.pth"  # Пример пути к сохранённой модели
+        # Пример пути к сохранённой модели
+        cfg.NET_G = "../data/models/netG_epoch_360.pth"
         cfg.TRAIN_FLAG = False
 
     now = datetime.datetime.now(dateutil.tz.tzlocal())
@@ -47,7 +56,8 @@ if __name__ == "__main__":
     gan_module = GANLitModule(stage=cfg.STAGE, output_dir=output_dir)
 
     if cfg.TRAIN_FLAG:
-        trainer = pl.Trainer(max_epochs=cfg.TRAIN_MAX_EPOCH, accelerator="gpu", devices=1)
+        trainer = pl.Trainer(max_epochs=cfg.TRAIN_MAX_EPOCH,
+                             accelerator="gpu", devices=1)
         trainer.fit(gan_module, datamodule=data_module)
     else:
         data_module.setup(stage='test')
